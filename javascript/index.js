2e4eabfec329f8492df57730a88499ecfd3d2e9b
Jacqueline Andreia D <songziyansong1@gmail.com>
2023-12-28 18:09:35 -0300

    adicionando section de contato com mapa e formulário
    - adicionei a section de contato com mapa e formulário de contato com campo de envio para o whatsapp já com as infos coletadas no value de cada campo do formulário. Footer finalizado

--- javascript/index.js
+++ javascript/index.js
@@ -1,40 +1,68 @@
+function redirectToWhatsApp() {
+
+  // Substitua '123456789' pelo número de telefone desejado
+  const numeroTel = '+5516988127756';
+
+  let usuario = document.getElementById('nome').value;
+  let emailUsuario = document.getElementById('email').value;
+  let tipoServico = document.getElementById('escolha').value;
+  let mensagemUsuario = document.getElementById('mensagem').value;
+
+
+  // Substitua 'Olá!' pela mensagem inicial (opcional)
+  const message = `Olá! meu nome é ${usuario}. Meu email é ${emailUsuario}. Escolhi o serviço ${tipoServico}. E entrei em contato pelo seguinte motivo: ${mensagemUsuario} `;
+
+  // Monta o link de mensagem do WhatsApp
+  const whatsappLink = 'https://wa.me/' + numeroTel + '/?text=' + encodeURIComponent(message);
+
+  // Redireciona para o link do WhatsApp
+  window.open(whatsappLink, '_blank');
+}
+
+
+
+const slider = document.querySelector('.container-carrossel');
+const botaoVoltar = document.getElementById('botao-voltar');
+const botaoAvancar = document.getElementById('botao-avancar');
+const slides = slider.querySelectorAll('.carrosel-slider');
 document.addEventListener('DOMContentLoaded', function () {
   const slider = document.querySelector('.container-carrossel');
   const slides = Array.from(slider.querySelectorAll('.carrosel-slider'));
 
+
   let currentSlide = 0;
 
   function showSlide(index) {
-      slides.forEach((slide, i) => {
-          if (i === index) {
-              slide.classList.add('on');
-          } else {
-              slide.classList.remove('on');
-          }
-      });
-  }
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('on');
+        } else {
+            slide.classList.remove('on');
+        }
+    });
+}
 
-  function nextSlide() {
-      currentSlide = (currentSlide + 1) % slides.length;
-      updateSliderPosition();
-      showSlide(currentSlide);
-  }
-
-  function prevSlide() {
-      currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-      updateSliderPosition();
-      showSlide(currentSlide);
-  }
-
-  function updateSliderPosition() {
-      const slideWidth = slides[currentSlide].offsetWidth;
-      const translateXValue = `-${currentSlide * slideWidth}px`;
-      slider.style.transform = `translateX(${translateXValue})`;
-  }
-
-  document.getElementById('botao-avancar').addEventListener('click', nextSlide);
-  document.getElementById('botao-voltar').addEventListener('click', prevSlide);
-
-  // Inicialmente, exibe o primeiro slide
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  updateSliderPosition();
   showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  updateSliderPosition();
+  showSlide(currentSlide);
+}
+
+function updateSliderPosition() {
+  const slideWidth = slides[currentSlide].offsetWidth;
+  const translateXValue = `-${currentSlide * slideWidth}px`;
+  slider.style.transform = `translateX(${translateXValue})`;
+}
+
+document.getElementById('botao-avancar').addEventListener('click', nextSlide);
+document.getElementById('botao-voltar').addEventListener('click', prevSlide);
+
+ // Inicialmente, exibe o primeiro slide
+ showSlide(currentSlide);
 });
